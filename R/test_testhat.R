f6defc1606775626059e860b95f8616a503edeb2
gorkang <gorkang@gmail.com>
2020-10-19 15:20:47 +0100

    loads all df_* targets automatically to avoid manual entry of automatic tests. Added warning in case we don't have tests for all df_* targets.

--- R/test_testhat.R
+++ R/test_testhat.R
@@ -1,4 +1,4 @@
-##' .. content for \description{} (no empty lines) ..
+##' Calls all the tests in 'test/testthat'
 ##'
 ##' .. content for \details{} ..
 ##'
@@ -7,29 +7,48 @@
 ##' @return
 ##' @author gorkang
 ##' @export
-test_testhat <- function(...) {
+test_testhat <- function(input_files_automatic_tests_str, ...) {
 
   # Load targets objects used in tests --------------------------------------
   
   argnames <- sys.call()
   arguments = lapply(argnames[-1], as.character) %>% unlist()
+  
+  
+  # Load targets
+  targets::tar_load(all_of(input_files_automatic_tests_str), envir = .GlobalEnv)
   targets::tar_load(all_of(arguments), envir = .GlobalEnv)
   
   
   # Print
   num_tests = list.files("tests/testthat/") %>% length()
-  cat(crayon::blue(crayon::underline(paste0("\n\nRunning ", num_tests, " tests:"))), " \n")
+  cat(crayon::blue(crayon::underline(crayon::bold(paste0("\n\n[Running ", num_tests, " tests]:", paste(rep(" ", 60), collapse = " ")), " \n"))))
+  # cat(crayon::blue(crayon::underline(paste(rep(" ", 48), collapse = " "))), " \n")
+
+  
+  # Check we have automatic tests for all tasks (each task should have a df_[SHORT_NAME_OF_TASK]) in _targets/outputs
+  automatic_tests_raw = list.files(path = "tests/testthat/", pattern="df_.*snapshot", full.names = FALSE, ignore.case = FALSE)
+  automatic_tests = gsub("test-(.*)_snapshot.R", "\\1", automatic_tests_raw)
+  missing_tests = input_files_automatic_tests_str[!input_files_automatic_tests_str %in% automatic_tests]
+  if (length(missing_tests) > 0)  cat(crayon::red(paste0("\n\n[WARNING]: Missing automatic tests: ")), paste(missing_tests, collapse = ","), "\n")
+  
   
 
   # Run all the tests ------------------------------------------------------
 
   testthat::test_dir(path = here::here("tests/testthat/"), env = .GlobalEnv, stop_on_failure = TRUE, reporter = StopReporter)#ProgressReporter
   
+  cat(crayon::blue(crayon::underline(crayon::bold(paste0("\n\n[END tests]:", paste(rep(" ", 65), collapse = " ")), " \n\n\n"))))
+  
 }
 
 
 
 # Creating automatic CHECKS
+# [TODO] This should be a helper function
+  # Create tests and DELETE old tests?
+  # Should tests take an actual snapshot of the object (.rds) dating it?
+
   # library(tidyverse)
   # GorkaFunctions::createtest_df_snapshot(DF_clean, n_char_variables_to_test = 10, n_num_variables_to_test = 3)
   # GorkaFunctions::createtest_df_snapshot(df_SBS, n_char_variables_to_test = 1, n_num_variables_to_test = 10)
