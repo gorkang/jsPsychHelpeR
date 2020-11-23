31a8da72abecd1cde4360ee4e59664dc7eeb46cc
gorkang <gorkang@gmail.com>
2020-11-23 10:24:49 +0000

    Should work with some instances of multiple inputs per page

--- R/read_data.R
+++ R/read_data.R
@@ -43,19 +43,34 @@ read_data <- function(input_files, anonymize = FALSE) {
         datetime = gsub("^[0-9]{1,99}_[a-zA-Z0-9]{1,99}_[a-zA-Z0-9]{1,99}_([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2})_.*.csv", "\\1", filename), # fecha
         
         stimulus = gsub('\\{"Q0":"|"\\}', '', stimulus), # Clean stimulus
-        responses = gsub('\\{"Q0":"|"\\}', '', responses), # Clean responses [REMEMBER: Only works with one response per screen]
-        responses = gsub('\u00A0', '', responses) # Remove non-breaking space (tools::showNonASCII(DF_raw$responses))
-      ) 
+        responses = gsub('"Q[0-9]"|&nbsp;|\u00A0', '', responses)
+        # responses = gsub('\\{"Q0":"|"\\}', '', responses), # Clean responses [REMEMBER: Only works with one response per screen]
+        # responses = gsub('\u00A0', '', responses) # Remove non-breaking space (tools::showNonASCII(DF_raw$responses))
+      ) %>% 
+      rowwise() %>% 
+      mutate(responses = lapply(regmatches(responses, gregexpr('(\").*?(\")', responses, perl = TRUE)), function(y) gsub("^\"|\"$", "", y)) %>% unlist() %>% paste(., collapse = "; ")) # Should deal with multiple responses (?)
+    
   
  
     
   # CHECK -------------------------------------------------------------------
   
     DF_duplicates = suppressMessages(DF_raw %>% janitor::get_dupes(-c(filename)))
-    input_files_duplicates = DF_duplicates %>% filter(success == TRUE) %>% distinct(filename) %>% pull(filename)
+    # suppressMessages(DF_raw %>% janitor::get_dupes(c(id, trialid))) %>% filter(trialid !="Instructions") %>% distinct(id) %>% pull(id)
+    # DF_raw%>% distinct(id) %>% pull(id)
+    # DF_raw %>% 
+    #   filter(trialid == "SDG_00") %>% 
+    #   janitor::get_dupes(c(id, project, experimento)) %>%
+    #   group_by(id) %>% 
+    #   sample_n(1) %>% 
+    #   pull(filename) %>% 
+    #   file.remove(paste0("data/", .))
+    # 
     
-    if (nrow(DF_duplicates) > 0) stop("[ERROR]: There are duplicates in the '/data' input files: ", paste(input_files_duplicates, collapse = ", "))
-
+    if (nrow(DF_duplicates) > 0) {
+      input_files_duplicates = DF_duplicates %>% filter(success == TRUE) %>% distinct(filename) %>% pull(filename)
+      stop("[ERROR]: There are duplicates in the '/data' input files: ", paste(input_files_duplicates, collapse = ", "))
+    }
   
   # Output of function ---------------------------------------------------------
     
