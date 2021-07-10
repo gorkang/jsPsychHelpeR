1d53d06c8da4a57e67dd89dbb8a1e6a7c5b2b256
G <gorkang@gmail.com>
2021-07-10 09:44:57 +0100

    work with TEST_001_1 trialids

--- R/create_diccionary_tasks.R
+++ R/create_diccionary_tasks.R
@@ -8,13 +8,19 @@
 ##' @author gorkang
 ##' @export
 create_diccionary_tasks <- function(DF_clean) {
-
-  DICCIONARY_tasks = DF_clean %>% 
-    mutate(`short_name: from trialid` = gsub("(.*)_[0-9]{1,3}", "\\1", trialid)) %>% 
-    # select(trialid, short_name)
+  
+  DICCIONARY_tasks = 
+    DF_clean %>% 
+    mutate(`short_name: from trialid` = 
+             case_when(
+               grepl("_[0-9]{1,3}_[0-9]{1,3}", trialid) ~ gsub("^(.*)_[0-9]{1,3}_[0-9]{1,3}", "\\1", trialid),
+               TRUE ~ gsub("^(.*)_[0-9]{1,3}", "\\1", trialid)
+             )
+    ) %>% 
+    # select(trialid, `short_name: from trialid`)
     distinct(experimento, `short_name: from trialid`) %>% 
     drop_na()
   
   write_csv(DICCIONARY_tasks, "outputs/data/DICCIONARY_tasks.csv")
-
+  
 }
