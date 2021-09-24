c2b006538979ce1a3f3874c804a810a9a0876ab3
G <gorkang@gmail.com>
2021-09-24 09:14:03 +0100

    comment out report to avoid error

--- targets/_targets_TEMPLATE.R
+++ targets/_targets_TEMPLATE.R
@@ -79,8 +79,8 @@ JOINS_HERE
   tar_render(descriptives_table1, "doc/descriptives_table1.Rmd", deployment = "main"),
 
   # Analisys report
-  tar_render(report_analysis, "doc/report_analysis.Rmd",
-             output_file = paste0("../outputs/reports/report_analysis.html")),
+  # tar_render(report_analysis, "doc/report_analysis.Rmd",
+  #            output_file = paste0("../outputs/reports/report_analysis.html")),
 
 
   # Models
