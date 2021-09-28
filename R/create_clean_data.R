b889c64d2775a362d3515e94c3772203e0ce91e4
G <gorkang@gmail.com>
2021-09-28 10:22:33 +0100

    Closes #10. Should work now with multi-select and multiple questions per screen

--- R/create_clean_data.R
+++ R/create_clean_data.R
@@ -16,14 +16,26 @@ create_clean_data <- function(DF_raw, save_output = TRUE) {
     filter(!trialid %in% c("Screen_WM", "Instructions")) %>%  # Delete instructions [TODO]: use regexp to clean instrucciones_NOMBRETEST
     filter(!grepl("Instructions", trialid, ignore.case = TRUE)) %>% 
     mutate(response = gsub('&nbsp;|\u00A0', '', response), # HTML space and invisible character
-           response = gsub(pattern = '\\[(.*?)"",""(.*?)\\]', replacement = "\\1; \\2", x = response, perl = TRUE), # In multi-select, output is: {\"\"Q0\"\":\"\"response1"",""response2\"\"} -> {\"\"Q0\"\":\"\"response1; response2\"\"}
-           response = gsub("\\[|\\]", "", response)) # Clean up remaining "[]"
+           # If we have [] in response, it is probably a multi-select (e.g. DEMOGR_19: "{\"\"Q0\"\":[\"\"&nbsp;Madre\"\",\"\"&nbsp;Hermanas\"\",\"\"&nbsp;Amigas cercanas\"\"]}")
+           # In this case, join all responses and separate by ;
+           response = 
+             case_when(
+               grepl("\\[", response) ~ gsub(pattern = '"",""', replacement = "; ", x = response, perl = TRUE), # In multi-select, output is: {\"\"Q0\"\":\"\"response1"",""response2\"\"} -> {\"\"Q0\"\":\"\"response1; response2\"\"}
+               TRUE ~ response
+             ),
+           response = gsub("\\[|\\]", "", response) # Clean up remaining "[]"
+    ) 
   
-  # DEBUG
-  # DF_clean_raw = DF_clean_raw %>% filter(experimento == "DEMOGR")
-  
-  # Creates one line per response for screens with multiple responses (e.g. Q0, Q1)
-  DF_clean = separate_responses(DF_clean_raw)  
+  # Creates one line per response
+  DF_clean = 
+    # Separate multiple responses and put questions in trialid. e.g. PRFBM
+    separate_responses(DF_clean_raw) %>% 
+    # Clean up remaining responses (e.g. DEMOGR_19)
+    mutate(
+      response = gsub('\\{"".*"":', "", response), # Get rid of {""Q0"":
+      response = gsub('\\}$', "", response), # Get rid of }
+      response = gsub(pattern = '""', replacement = "", x = response, perl = TRUE) # Remaining double quotes""
+    )
   
   
   # Save files --------------------------------------------------------------
