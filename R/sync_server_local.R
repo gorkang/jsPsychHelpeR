af9a965dba834a22403f317ca151abb3b1be4a75
G <gorkang@gmail.com>
2022-02-21 10:32:41 +0000

    add only_test parameter

--- R/sync_server_local.R
+++ R/sync_server_local.R
@@ -1,11 +1,18 @@
 # Sync server with local Maker
-sync_server_local <- function(server_folder, local_folder, direction) {
+sync_server_local <- function(server_folder, local_folder, direction, only_test = TRUE) {
   
   # DEBUG
-  # server_folder = "test/999/"
+  # server_folder = "test/FONDECYT2021/"
   # local_folder = "canonical_protocol_DEV/"
   # direction = "server_to_local"
   
+  if (only_test == TRUE) {
+    extra_message = paste0(cli::col_red("THIS IS A dry-run"))
+    dry_run = " --dry-run "
+  } else {
+    extra_message = ""
+    dry_run = ""
+  }
   
   local_folder = normalizePath(here::here(local_folder))
   
@@ -31,13 +38,14 @@ sync_server_local <- function(server_folder, local_folder, direction) {
   # CHECK -------------------------------------------------------------------
   
   if (direction == "server_to_local") {
-    message_text = paste0(cli::col_yellow("Will sync: "), cli::col_silver("cscn.uai.cl/", server_folder, " -->> ", local_folder))
+    message_text = paste0(cli::col_yellow("Will sync: "), cli::col_silver("cscn.uai.cl/", server_folder, " -->> ", local_folder), "\n", extra_message)
   } else if (direction == "local_to_server") {
-    message_text = paste0(cli::col_yellow("Will sync: "), cli::col_silver(local_folder, " -->> ", "cscn.uai.cl/", server_folder))
+    message_text = paste0(cli::col_yellow("Will sync: "), cli::col_silver(local_folder, " -->> ", "cscn.uai.cl/", server_folder), "\n", extra_message)
   } else {
     cli::cli_text(cli::col_red("{cli::symbol$cross} "), "direction should be either 'server_to_local' or 'local_to_server'")
     stop()
   }
+  
   out <- utils::menu(c("yes", "no"), title = cat(message_text))
   
   
@@ -51,9 +59,9 @@ sync_server_local <- function(server_folder, local_folder, direction) {
     
     if (direction == "server_to_local") {
       
-      # DOWNLOAD from server to local folder
+      # DOWNLOAD server to local
       system(
-        paste0('sshpass -p ', list_credentials$value$password, ' rsync -av --rsh=ssh ', 
+        paste0('sshpass -p ', list_credentials$value$password, ' rsync -av ', dry_run, ' --rsh=ssh ', 
                list_credentials$value$user, "@", list_credentials$value$IP, ":", list_credentials$value$main_FOLDER, server_folder, '/ ',
                here::here(local_folder), '/ '
         )
@@ -62,9 +70,9 @@ sync_server_local <- function(server_folder, local_folder, direction) {
       
     } else if (direction == "local_to_server") {
       
-      # UPLOAD from local folder to server
+      # UPLOAD local to server
       system(
-        paste0('sshpass -p ', list_credentials$value$password, ' rsync -av --rsh=ssh ', 
+        paste0('sshpass -p ', list_credentials$value$password, ' rsync -av ', dry_run, ' --rsh=ssh ', 
                here::here(local_folder), '/ ',
                list_credentials$value$user, "@", list_credentials$value$IP, ":", list_credentials$value$main_FOLDER, server_folder, '/ '
         )
@@ -77,4 +85,4 @@ sync_server_local <- function(server_folder, local_folder, direction) {
   }
   
 }
-# sync_server_local(server_folder = "test/999/", local_folder = "canonical_protocol_DEV/", direction = "server_to_local")
+# sync_server_local(server_folder = "test/FONDECYT2021/", local_folder = "canonical_protocol_DEV/", direction = "server_to_local")
