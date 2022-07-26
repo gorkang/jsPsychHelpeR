d12c18e18792a358d0b47a176516237f07ded32c
Gorka Navarrete <gorkang@gmail.com>
2022-07-26 18:03:25 +0100

    Add RELd columns to DF_analysis

--- R/create_DF_analysis.R
+++ R/create_DF_analysis.R
@@ -12,7 +12,7 @@ create_DF_analysis <- function(DF_joined, last_task, save_output = TRUE) {
   # debug_function("create_DF_analysis")
   # Selects all STDt, STDd, DIRt and DIRd scales
   # all_scales also used in "tests/testthat/test-missing_data.R"
-  all_scales = grep(".*_DIRt$|.*_STDt$|.*_DIRd$|.*STDd$", names(DF_joined), value = TRUE, perl = TRUE)
+  all_scales = grep(".*_DIRt$|.*_STDt$|.*_DIRd$|.*RELd$|.*STDd$", names(DF_joined), value = TRUE, perl = TRUE)
     
   DF_analysis = 
     DF_joined %>% 
