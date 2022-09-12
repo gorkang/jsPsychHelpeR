a56c083de06e1cc5f9c6e150573b5a099708b0ed
Gorka Navarrete <gorkang@gmail.com>
2022-09-12 12:56:28 +0100

    fix trialid names

--- R_tasks/prepare_fauxPasEv.R
+++ R_tasks/prepare_fauxPasEv.R
@@ -65,115 +65,115 @@ prepare_fauxPasEv <- function(DF_clean, short_name_scale_str) {
         # Pregunta 1 / Item 2/9
           # Historias con Faux Pas
           # Si!
-          trialid %in% c('faux_pas_ev_011', 'faux_pas_ev_029', 'faux_pas_ev_056',
-                         'faux_pas_ev_092', 'faux_pas_ev_101', 'faux_pas_ev_110', 
-                         'faux_pas_ev_119', 'faux_pas_ev_128', 'faux_pas_ev_137', 
-                         'faux_pas_ev_155') & RAW == 'Si' ~ 1,
+          trialid %in% c('fauxPasEv_011', 'fauxPasEv_029', 'fauxPasEv_056',
+                         'fauxPasEv_092', 'fauxPasEv_101', 'fauxPasEv_110', 
+                         'fauxPasEv_119', 'fauxPasEv_128', 'fauxPasEv_137', 
+                         'fauxPasEv_155') & RAW == 'Si' ~ 1,
           
           # No!
-          trialid %in% c('faux_pas_ev_011', 'faux_pas_ev_029', 'faux_pas_ev_056', 
-                         'faux_pas_ev_092', 'faux_pas_ev_101', 'faux_pas_ev_110', 
-                         'faux_pas_ev_119', 'faux_pas_ev_128', 'faux_pas_ev_137',
-                         'faux_pas_ev_155') & RAW == 'No' ~ 0,
+          trialid %in% c('fauxPasEv_011', 'fauxPasEv_029', 'fauxPasEv_056', 
+                         'fauxPasEv_092', 'fauxPasEv_101', 'fauxPasEv_110', 
+                         'fauxPasEv_119', 'fauxPasEv_128', 'fauxPasEv_137',
+                         'fauxPasEv_155') & RAW == 'No' ~ 0,
           
           # Resto de preguntas con respuestas especificas
-          trialid == 'faux_pas_ev_011' & RAW == 'No' ~ 2, # historia 1
-          trialid == 'faux_pas_ev_021' & RAW == 'Si' ~ 1, # historia 2
-          trialid == 'faux_pas_ev_022' & RAW == 'Sara' ~ 1, # historia 2
-          trialid == 'faux_pas_ev_025' & RAW == 'No' ~ 1, # historia 2
-          trialid == 'faux_pas_ev_031' & RAW == 'No' ~ 2, # historia 3
-          trialid == 'faux_pas_ev_041' & RAW == 'Si' ~ 1, # historia 4
-          trialid == 'faux_pas_ev_042' & RAW == 'Alicia' ~ 1, # historia 4
-          trialid == 'faux_pas_ev_045' & RAW == 'No' ~ 1, # historia 4
-          trialid == 'faux_pas_ev_051' & RAW == 'No' ~ 2, # historia 5
-          trialid == 'faux_pas_ev_061' & RAW == 'No' ~ 2, # historia 6
-          trialid == 'faux_pas_ev_071' & RAW == 'Si' ~ 1, # historia 7
-          trialid == 'faux_pas_ev_072' & RAW == 'La vecina María' ~ 1, # historia 7
-          trialid == 'faux_pas_ev_075' & RAW == 'No' ~ 1, # historia 7
-          trialid == 'faux_pas_ev_081' & RAW == 'No' ~ 2, # historia 8
-          trialid == 'faux_pas_ev_091' & RAW == 'No' ~ 2, # historia 9
-          trialid == 'faux_pas_ev_101' & RAW == 'No' ~ 2, # historia 10
-          trialid == 'faux_pas_ev_111' & RAW == 'Si' ~ 1, # historia 11
-          trialid == 'faux_pas_ev_112' & RAW == 'Roberto, el ingeniero' ~ 1, # historia 11
-          trialid == 'faux_pas_ev_115' & RAW == 'No' ~ 1, # historia 11
-          trialid == 'faux_pas_ev_121' & RAW == 'Si' ~ 1, # historia 12
-          trialid == 'faux_pas_ev_122' & RAW == 'Jose' ~ 1, # historia 12
-          trialid == 'faux_pas_ev_122' & RAW == 'Pedro' ~ 1, # historia 12
-          trialid == 'faux_pas_ev_125' & RAW == 'No' ~ 1, # historia 12
-          trialid == 'faux_pas_ev_131' & RAW == 'Si' ~ 1, # historia 13
-          trialid == 'faux_pas_ev_132' & RAW == 'Sergio, el primo de karina' ~ 1, # historia 13
-          trialid == 'faux_pas_ev_135' & RAW == 'No' ~ 1, # historia 13
-          trialid == 'faux_pas_ev_141' & RAW == 'Si' ~ 1, # historia 14
-          trialid == 'faux_pas_ev_142' & RAW == 'Ana ' ~ 1, # historia 14
-          trialid == 'faux_pas_ev_145' & RAW == 'No' ~ 1, # historia 14
-          trialid == 'faux_pas_ev_151' & RAW == 'Si' ~ 1, # historia 15
-          trialid == 'faux_pas_ev_152' & RAW == 'Ana' ~ 1, # historia 15
-          trialid == 'faux_pas_ev_155' & RAW == 'No' ~ 1, # historia 15
-          trialid == 'faux_pas_ev_161' & RAW == 'Si' ~ 1, # historia 16
-          trialid == 'faux_pas_ev_162' & RAW == 'Tito' ~ 1, # historia 16
-          trialid == 'faux_pas_ev_165' & RAW == 'No' ~ 1, # historia 16
-          trialid == 'faux_pas_ev_171' & RAW == 'No' ~ 2, # historia 17
-          trialid == 'faux_pas_ev_181' & RAW == 'Si' ~ 1, # historia 18
-          trialid == 'faux_pas_ev_182' & RAW == 'Clara' ~ 1, # historia 18
-          trialid == 'faux_pas_ev_185' & RAW == 'No' ~ 1, # historia 18
-          trialid == 'faux_pas_ev_191' & RAW == 'No' ~ 2, # historia 19
-          trialid == 'faux_pas_ev_201' & RAW == 'No' ~ 2, # historia 20
+          trialid == 'fauxPasEv_011' & RAW == 'No' ~ 2, # historia 1
+          trialid == 'fauxPasEv_021' & RAW == 'Si' ~ 1, # historia 2
+          trialid == 'fauxPasEv_022' & RAW == 'Sara' ~ 1, # historia 2
+          trialid == 'fauxPasEv_025' & RAW == 'No' ~ 1, # historia 2
+          trialid == 'fauxPasEv_031' & RAW == 'No' ~ 2, # historia 3
+          trialid == 'fauxPasEv_041' & RAW == 'Si' ~ 1, # historia 4
+          trialid == 'fauxPasEv_042' & RAW == 'Alicia' ~ 1, # historia 4
+          trialid == 'fauxPasEv_045' & RAW == 'No' ~ 1, # historia 4
+          trialid == 'fauxPasEv_051' & RAW == 'No' ~ 2, # historia 5
+          trialid == 'fauxPasEv_061' & RAW == 'No' ~ 2, # historia 6
+          trialid == 'fauxPasEv_071' & RAW == 'Si' ~ 1, # historia 7
+          trialid == 'fauxPasEv_072' & RAW == 'La vecina María' ~ 1, # historia 7
+          trialid == 'fauxPasEv_075' & RAW == 'No' ~ 1, # historia 7
+          trialid == 'fauxPasEv_081' & RAW == 'No' ~ 2, # historia 8
+          trialid == 'fauxPasEv_091' & RAW == 'No' ~ 2, # historia 9
+          trialid == 'fauxPasEv_101' & RAW == 'No' ~ 2, # historia 10
+          trialid == 'fauxPasEv_111' & RAW == 'Si' ~ 1, # historia 11
+          trialid == 'fauxPasEv_112' & RAW == 'Roberto, el ingeniero' ~ 1, # historia 11
+          trialid == 'fauxPasEv_115' & RAW == 'No' ~ 1, # historia 11
+          trialid == 'fauxPasEv_121' & RAW == 'Si' ~ 1, # historia 12
+          trialid == 'fauxPasEv_122' & RAW == 'Jose' ~ 1, # historia 12
+          trialid == 'fauxPasEv_122' & RAW == 'Pedro' ~ 1, # historia 12
+          trialid == 'fauxPasEv_125' & RAW == 'No' ~ 1, # historia 12
+          trialid == 'fauxPasEv_131' & RAW == 'Si' ~ 1, # historia 13
+          trialid == 'fauxPasEv_132' & RAW == 'Sergio, el primo de karina' ~ 1, # historia 13
+          trialid == 'fauxPasEv_135' & RAW == 'No' ~ 1, # historia 13
+          trialid == 'fauxPasEv_141' & RAW == 'Si' ~ 1, # historia 14
+          trialid == 'fauxPasEv_142' & RAW == 'Ana ' ~ 1, # historia 14
+          trialid == 'fauxPasEv_145' & RAW == 'No' ~ 1, # historia 14
+          trialid == 'fauxPasEv_151' & RAW == 'Si' ~ 1, # historia 15
+          trialid == 'fauxPasEv_152' & RAW == 'Ana' ~ 1, # historia 15
+          trialid == 'fauxPasEv_155' & RAW == 'No' ~ 1, # historia 15
+          trialid == 'fauxPasEv_161' & RAW == 'Si' ~ 1, # historia 16
+          trialid == 'fauxPasEv_162' & RAW == 'Tito' ~ 1, # historia 16
+          trialid == 'fauxPasEv_165' & RAW == 'No' ~ 1, # historia 16
+          trialid == 'fauxPasEv_171' & RAW == 'No' ~ 2, # historia 17
+          trialid == 'fauxPasEv_181' & RAW == 'Si' ~ 1, # historia 18
+          trialid == 'fauxPasEv_182' & RAW == 'Clara' ~ 1, # historia 18
+          trialid == 'fauxPasEv_185' & RAW == 'No' ~ 1, # historia 18
+          trialid == 'fauxPasEv_191' & RAW == 'No' ~ 2, # historia 19
+          trialid == 'fauxPasEv_201' & RAW == 'No' ~ 2, # historia 20
           
           
           # Historias sin Faux Pas
-          trialid == 'faux_pas_ev_002' & RAW == 'No' ~ 2,
-          trialid == 'faux_pas_ev_020' & RAW == 'No' ~ 2,
-          trialid == 'faux_pas_ev_038' & RAW == 'No' ~ 2,
-          trialid == 'faux_pas_ev_047' & RAW == 'No' ~ 2,
-          trialid == 'faux_pas_ev_065' & RAW == 'No' ~ 2,
-          trialid == 'faux_pas_ev_074' & RAW == 'No' ~ 2,
-          trialid == 'faux_pas_ev_083' & RAW == 'No' ~ 2,
-          trialid == 'faux_pas_ev_146' & RAW == 'No' ~ 2,
+          trialid == 'fauxPasEv_002' & RAW == 'No' ~ 2,
+          trialid == 'fauxPasEv_020' & RAW == 'No' ~ 2,
+          trialid == 'fauxPasEv_038' & RAW == 'No' ~ 2,
+          trialid == 'fauxPasEv_047' & RAW == 'No' ~ 2,
+          trialid == 'fauxPasEv_065' & RAW == 'No' ~ 2,
+          trialid == 'fauxPasEv_074' & RAW == 'No' ~ 2,
+          trialid == 'fauxPasEv_083' & RAW == 'No' ~ 2,
+          trialid == 'fauxPasEv_146' & RAW == 'No' ~ 2,
 
           # Preguntas que NO se puntuan
-          trialid %in% c('faux_pas_ev_012', 'faux_pas_ev_015', 'faux_pas_ev_018',
-                         'faux_pas_ev_027', 'faux_pas_ev_028', 'faux_pas_ev_032',
-                         'faux_pas_ev_035', 'faux_pas_ev_052', 'faux_pas_ev_055',
-                         'faux_pas_ev_062', 'faux_pas_ev_065', 'faux_pas_ev_082',
-                         'faux_pas_ev_085', 'faux_pas_ev_092', 'faux_pas_ev_095',
-                         'faux_pas_ev_102', 'faux_pas_ev_105', 'faux_pas_ev_157',
-                         'faux_pas_ev_158', 'faux_pas_ev_172', 'faux_pas_ev_175',
-                         'faux_pas_ev_192', 'faux_pas_ev_195', 'faux_pas_ev_202',
-                         'faux_pas_ev_205') ~ 0, 
+          trialid %in% c('fauxPasEv_012', 'fauxPasEv_015', 'fauxPasEv_018',
+                         'fauxPasEv_027', 'fauxPasEv_028', 'fauxPasEv_032',
+                         'fauxPasEv_035', 'fauxPasEv_052', 'fauxPasEv_055',
+                         'fauxPasEv_062', 'fauxPasEv_065', 'fauxPasEv_082',
+                         'fauxPasEv_085', 'fauxPasEv_092', 'fauxPasEv_095',
+                         'fauxPasEv_102', 'fauxPasEv_105', 'fauxPasEv_157',
+                         'fauxPasEv_158', 'fauxPasEv_172', 'fauxPasEv_175',
+                         'fauxPasEv_192', 'fauxPasEv_195', 'fauxPasEv_202',
+                         'fauxPasEv_205') ~ 0, 
           
           # Para correccion manual
-          trialid %in% c('faux_pas_ev_013', 'faux_pas_ev_014', 'faux_pas_ev_016',
-                         'faux_pas_ev_017', 'faux_pas_ev_023', 'faux_pas_ev_024', 
-                         'faux_pas_ev_026', 'faux_pas_ev_033', 'faux_pas_ev_034', 
-                         'faux_pas_ev_036', 'faux_pas_ev_037', 'faux_pas_ev_038',
-                         'faux_pas_ev_043', 'faux_pas_ev_044', 'faux_pas_ev_046',
-                         'faux_pas_ev_047', 'faux_pas_ev_048', 'faux_pas_ev_053',
-                         'faux_pas_ev_054', 'faux_pas_ev_056', 'faux_pas_ev_057',
-                         'faux_pas_ev_058', 'faux_pas_ev_063', 'faux_pas_ev_064', 
-                         'faux_pas_ev_066', 'faux_pas_ev_067', 'faux_pas_ev_068',
-                         'faux_pas_ev_073', 'faux_pas_ev_074', 'faux_pas_ev_076',
-                         'faux_pas_ev_077', 'faux_pas_ev_078', 'faux_pas_ev_083', 
-                         'faux_pas_ev_084', 'faux_pas_ev_086', 'faux_pas_ev_087', 
-                         'faux_pas_ev_088', 'faux_pas_ev_093', 'faux_pas_ev_094',
-                         'faux_pas_ev_096', 'faux_pas_ev_097', 'faux_pas_ev_098',
-                         'faux_pas_ev_103', 'faux_pas_ev_104', 'faux_pas_ev_106',
-                         'faux_pas_ev_107', 'faux_pas_ev_108', 'faux_pas_ev_113',
-                         'faux_pas_ev_114', 'faux_pas_ev_116', 'faux_pas_ev_117',
-                         'faux_pas_ev_118', 'faux_pas_ev_123', 'faux_pas_ev_124', 
-                         'faux_pas_ev_126', 'faux_pas_ev_127', 'faux_pas_ev_128',
-                         'faux_pas_ev_133', 'faux_pas_ev_134', 'faux_pas_ev_136', 
-                         'faux_pas_ev_137', 'faux_pas_ev_138', 'faux_pas_ev_143', 
-                         'faux_pas_ev_144', 'faux_pas_ev_146', 'faux_pas_ev_147', 
-                         'faux_pas_ev_148', 'faux_pas_ev_153', 'faux_pas_ev_154', 
-                         'faux_pas_ev_156', 'faux_pas_ev_163', 'faux_pas_ev_164', 
-                         'faux_pas_ev_166', 'faux_pas_ev_167', 'faux_pas_ev_168',
-                         'faux_pas_ev_173', 'faux_pas_ev_174', 'faux_pas_ev_176',
-                         'faux_pas_ev_177', 'faux_pas_ev_178', 'faux_pas_ev_183', 
-                         'faux_pas_ev_184', 'faux_pas_ev_186', 'faux_pas_ev_187',
-                         'faux_pas_ev_188', 'faux_pas_ev_193', 'faux_pas_ev_194', 
-                         'faux_pas_ev_196', 'faux_pas_ev_197', 'faux_pas_ev_198', 
-                         'faux_pas_ev_203', 'faux_pas_ev_204', 'faux_pas_ev_206', 
-                        'faux_pas_ev_207', 'faux_pas_ev_208') ~ 123456789,
+          trialid %in% c('fauxPasEv_013', 'fauxPasEv_014', 'fauxPasEv_016',
+                         'fauxPasEv_017', 'fauxPasEv_023', 'fauxPasEv_024', 
+                         'fauxPasEv_026', 'fauxPasEv_033', 'fauxPasEv_034', 
+                         'fauxPasEv_036', 'fauxPasEv_037', 'fauxPasEv_038',
+                         'fauxPasEv_043', 'fauxPasEv_044', 'fauxPasEv_046',
+                         'fauxPasEv_047', 'fauxPasEv_048', 'fauxPasEv_053',
+                         'fauxPasEv_054', 'fauxPasEv_056', 'fauxPasEv_057',
+                         'fauxPasEv_058', 'fauxPasEv_063', 'fauxPasEv_064', 
+                         'fauxPasEv_066', 'fauxPasEv_067', 'fauxPasEv_068',
+                         'fauxPasEv_073', 'fauxPasEv_074', 'fauxPasEv_076',
+                         'fauxPasEv_077', 'fauxPasEv_078', 'fauxPasEv_083', 
+                         'fauxPasEv_084', 'fauxPasEv_086', 'fauxPasEv_087', 
+                         'fauxPasEv_088', 'fauxPasEv_093', 'fauxPasEv_094',
+                         'fauxPasEv_096', 'fauxPasEv_097', 'fauxPasEv_098',
+                         'fauxPasEv_103', 'fauxPasEv_104', 'fauxPasEv_106',
+                         'fauxPasEv_107', 'fauxPasEv_108', 'fauxPasEv_113',
+                         'fauxPasEv_114', 'fauxPasEv_116', 'fauxPasEv_117',
+                         'fauxPasEv_118', 'fauxPasEv_123', 'fauxPasEv_124', 
+                         'fauxPasEv_126', 'fauxPasEv_127', 'fauxPasEv_128',
+                         'fauxPasEv_133', 'fauxPasEv_134', 'fauxPasEv_136', 
+                         'fauxPasEv_137', 'fauxPasEv_138', 'fauxPasEv_143', 
+                         'fauxPasEv_144', 'fauxPasEv_146', 'fauxPasEv_147', 
+                         'fauxPasEv_148', 'fauxPasEv_153', 'fauxPasEv_154', 
+                         'fauxPasEv_156', 'fauxPasEv_163', 'fauxPasEv_164', 
+                         'fauxPasEv_166', 'fauxPasEv_167', 'fauxPasEv_168',
+                         'fauxPasEv_173', 'fauxPasEv_174', 'fauxPasEv_176',
+                         'fauxPasEv_177', 'fauxPasEv_178', 'fauxPasEv_183', 
+                         'fauxPasEv_184', 'fauxPasEv_186', 'fauxPasEv_187',
+                         'fauxPasEv_188', 'fauxPasEv_193', 'fauxPasEv_194', 
+                         'fauxPasEv_196', 'fauxPasEv_197', 'fauxPasEv_198', 
+                         'fauxPasEv_203', 'fauxPasEv_204', 'fauxPasEv_206', 
+                        'fauxPasEv_207', 'fauxPasEv_208') ~ 123456789,
           
           is.na(RAW) ~ NA_real_,
           grepl(items_to_ignore, trialid) ~ NA_real_,
@@ -218,6 +218,9 @@ prepare_fauxPasEv <- function(DF_clean, short_name_scale_str) {
     writexl::write_xlsx(here::here(manual_correction_output))
   
   
+  # CHECK FILES EXIST!!! IF NOT, JUMP ALL and give message
+  
+  
   # Read manual correction file ---
   if (file.exists(manual_correction_input)) {
     # cli::cli_par()
@@ -226,6 +229,7 @@ prepare_fauxPasEv <- function(DF_clean, short_name_scale_str) {
     
     DF_manual_correction = readxl::read_excel(here::here("data/manual_correction/fauxPasEv_manual_correction.xlsx")) 
     nrow_output = nrow(readxl::read_excel(manual_correction_output))
+    
     nrow_input = nrow(DF_manual_correction)
     
     nrow_uncorrected = DF_manual_correction %>% filter(DIR == 123456789) %>% nrow()
