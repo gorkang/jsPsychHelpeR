861ea13dc50052f1e8156ca4498d5d20ecb21537
G <gorkang@gmail.com>
2022-10-14 17:49:32 +0100

    pre-process data, asign permission to emails

--- admin/sync_data_active_protocols.R
+++ admin/sync_data_active_protocols.R
@@ -1,17 +1,42 @@
 # Sync and zip data folder of active protocols
 
-### THIS DOCUMENT RUNS DAILY: /etc/cron.daily/gorka-sync_data_jspsychHelpeR ###
+  # CHECKS active folders: https://docs.google.com/spreadsheets/d/13xX8aGhKmfz8zVvNuCMDnS6L6Dy6T1NlA6nTOqZuErA/edit#gid=0
+  # ZIPS to jsPsychR/SHARED-data/
+  # Processes and zips DF_raw and DF_clean
+  # Individual project folders will be shared with the IP for each project
 
-# CHECKS active folders: https://docs.google.com/spreadsheets/d/13xX8aGhKmfz8zVvNuCMDnS6L6Dy6T1NlA6nTOqZuErA/edit#gid=0
-# ZIPS to jsPsychR/SHARED-data/
-# Individual project folders will be shared with the IP for each project
+
+
+### THIS DOCUMENT RUNS DAILY via anacron ###
+
+# FILE: /etc/cron.daily/gorka-sync_data_jspsychHelpeR -----------------------
+
+  # #!/bin/sh
+  # 
+  # # If started as root, then re-start as user "emrys":
+  # if [ "$(id -u)" -eq 0 ]; then
+  # exec sudo -H -u emrys $0 "$@"
+  # echo "This is never reached.";
+  # fi
+  # 
+  # cd /home/emrys/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/jsPsychR/jsPsychHelpeR/admin/ && Rscript sync_data_active_protocols.R
+  # 
+  # exit 0;
+
+# END FILE /etc/cron.daily/gorka-sync_data_jspsychHelpeR --------------------
+
+
+# TO TEST THIS WILL WORK WHEN RUN DAILY: 
+# system("cd /home/emrys/gorkang@gmail.com/RESEARCH/PROYECTOS-Code/jsPsychR/jsPsychHelpeR/admin/ && Rscript sync_data_active_protocols.R")
 
 
 # Sources and parameters --------------------------------------------------
-
-  source(here::here("R/helper_functions.R"))
-  source(here::here("R/sync_server_local.R"))
   
+  suppressPackageStartupMessages(source(here::here("_targets_packages.R")))
+  invisible(lapply(list.files(here::here("./R"), full.names = TRUE, pattern = ".R$"), source))
+  
+  # source(here::here("R/helper_functions.R"))
+  # source(here::here("R/sync_server_local.R"))
 
   google_sheet_ID = "1eE0xrk_DGIOShhWjqKaKhk0NIeb-7k0eoFG93822-Ho" 
   cli::cli_h1("Reading https://docs.google.com/spreadsheets/d/{google_sheet_ID}/edit#gid=0")
@@ -41,12 +66,47 @@
   1:length(PIDs) |> 
     purrr::walk( ~ {
       
-      # .x = 1
-      cli::cli_h1("Downloading data for project {PIDs[.x]}")
-      
+      cli::cli_h1("Project {PIDs[.x]}")
+
+      # Clean outputs/data/
+      cli::cli_h2("Clean up outputs/data")
+      FILES = list.files(here::here("outputs/data/"), full.names = TRUE, pattern = "DF_clean|DF_raw")
+      file.remove(FILES)
+      Sys.sleep(5) # Give some time to sync changes to google drive
+            
+      # Download data
+      cli::cli_h2("Downloading data for project {PIDs[.x]}")
       get_zip(pid = PIDs[.x], what = "data")
       
-      })
+      # Read ZIP and process data
+      zip_name = here::here(paste0("../SHARED-data/", PIDs[.x], "/", PIDs[.x], ".zip"))
+      
+      
+      if (file.exists(zip_name)) {
+      
+        # Process data (DF_raw and DF_clean)
+        cli::cli_h2("Processing data for project {PIDs[.x]}")
+    
+        DF_raw = read_data(input_files = zip_name)
+        DF_clean = create_clean_data(DF_raw)
+          
+        
+  
+        # Copy processed files to destination folder
+        cli::cli_h2("Copy output processed files for project {PIDs[.x]}")
+        FILES = list.files(here::here("outputs/data/"), full.names = TRUE, pattern = "DF_clean|DF_raw")
+        destination = paste0(dirname(zip_name), "/processed/")
+        if (!dir.exists(destination)) dir.create(destination)
+        file.copy(from = FILES, to = paste0(destination, basename(FILES)), overwrite = TRUE)
+        
+        # Create zip with process data
+        zip_files(folder_files = destination, 
+                  zip_name = paste0(destination, "/", "processed.zip"), 
+                  remove_files = TRUE)
+      
+      }
+      
+    })
 
   
 
@@ -55,20 +115,29 @@
   # TODO: FOR NOW, do this manually. Make sure all emails are OK
   
   # DF_resumen_clean
+  # EMAIL = "name-surname@university.edu.com"
+  # grepl(pattern = "\\b[-A-Za-z0-9_.%]+\\@[-A-Za-z0-9_.%]+\\.[A-Za-z]+", EMAIL)
+  # grepl(pattern = "^[[:alnum:].-_]+@[[:alnum:].-]+$", EMAIL)
   
-  # 1:length(PIDs) |>
-  #   purrr::walk( ~ {
-  # 
-  #     DF_permisos = DF_resumen_clean |> dplyr::filter(ID == PIDs[.x])
-  # 
-  #     IS_EMAIL_regex = "^[[:alnum:].-_]+@[[:alnum:].-]+$"
-  # 
-  #     # CHECK if contacto looks like a single well formed email
-  #     if (grepl(pattern = IS_EMAIL_regex, DF_permisos$contacto)) {
-  #       # Set permissions, only if not ADMIN and does not already have permissions
-  #       set_permissions_google_drive(pid = DF_permisos$ID, email_IP = DF_permisos$contacto)
-  #     } else {
-  #       cli::cli_alert_danger("{DF_permisos$contacto} does NOT look like a proper email")
-  #     }
-  # 
-  #   })
+  
+  1:length(PIDs) |>
+    purrr::walk( ~ {
+
+      DF_permisos = DF_resumen_clean |> dplyr::filter(ID == PIDs[.x])
+
+      IS_EMAIL_regex = "\\b[-A-Za-z0-9_.%]+\\@[-A-Za-z0-9_.%]+\\.[A-Za-z]+"
+      
+      # CHECK if contacto looks like a single well formed email
+      if (grepl(pattern = IS_EMAIL_regex, DF_permisos$contacto)) {
+        
+        # Set permissions, only if not ADMIN and does not already have permissions
+        set_permissions_google_drive(pid = DF_permisos$ID, email_IP = DF_permisos$contacto)
+        
+        cli::cli_alert_success("Asignados permisos sobre pid: {DF_permisos$ID} a {DF_permisos$contacto}")
+        
+        
+      } else {
+        cli::cli_alert_danger("{DF_permisos$contacto} does NOT look like a proper email")
+      }
+
+    })
