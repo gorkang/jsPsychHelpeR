347d29f2d9dd29bfb0055d5b25d85e47272711c2
G <gorkang@gmail.com>
2022-11-04 10:55:57 +0000

    use read_csv_or_zip to read data

--- R/read_data.R
+++ R/read_data.R
@@ -18,30 +18,7 @@ read_data <- function(input_files, anonymize = FALSE, save_output = TRUE, worker
   # workers = 1
   
   # We accept either a single zip file or multiple csv files
-    # The function list_input_files() checks that is the case, so input_files here SHOULD be one of these two
-  all_csvs = all(grepl("\\.csv", input_files))
-  all_zips = all(grepl("\\.zip", input_files))
-  
-  
-  # Read file/s
-  if (all_csvs) {
-    
-    # TEST and remove empty files (size < 100 bytes)
-    empty_files = file.info(input_files) %>% as_tibble(rownames = "files") %>% filter(size < 100)
-    if (nrow(empty_files) > 0) cli::cli_alert_warning("There are {length(empty_files)} empty input files (size < 100 bytes)")
-    input_files = input_files[!input_files %in% empty_files$files]
-    
-    DF_raw_read = purrr::map_dfr(input_files %>% set_names(basename(.)), data.table::fread, .id = "filename", colClasses = 'character', encoding = 'UTF-8', nThread = as.numeric(workers)) %>% as_tibble()
-    # colClasses = c(response = "character")
-    
-  } else if (all_zips) {
-    
-    # Unzips to temp folder, reads files and deletes temp folder
-    DF_raw_read = read_zips(input_files)  
-    
-  } else {
-    cli::cli_abort("Something wrong in read_data(). Are input files all csv files or a single zip file?")
-  }
+  DF_raw_read = read_csv_or_zip(input_files, workers = workers)
   
   
   # In some old jsPsych plugins, we have a responses column instead of response
