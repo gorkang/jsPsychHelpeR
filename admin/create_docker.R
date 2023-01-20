9abd24905d61202b58ab731cd8a490e7ef024d23
Gorka Navarrete <gorkang@gmail.com>
2023-01-20 19:09:46 +0000

    update docker

--- admin/create_docker.R
+++ admin/create_docker.R
@@ -1,7 +1,15 @@
 # This creates a Dockerfile in the project's main folder, builds the docker container and runs it
 
+PID = 999
+
 source("admin/helper-scripts-admin.R")
-create_dockerfile(PID = 999)
-system("docker build -t project .")
-system("docker run --rm -d --name jspsychhelper -v ~/Downloads/jsPsychHelpeR/outputs:/home/project/jsPsychHelpeR/outputs:rw project")
+create_dockerfile(PID = PID)
+system(paste0("docker build -t pid", PID, " ."))
+system(paste0("docker run --rm -d --name jspsychhelper -v ~/Downloads/jsPsychHelpeR/outputs:/home/project/jsPsychHelpeR/outputs:rw ", PID))
 list.files("~/Downloads/jsPsychHelpeR/outputs", recursive = TRUE)
+
+# TO SHARE
+docker save pid999 | zip > pid999.tar.zip # regular 2.1 GB, zip 944 MB
+
+# UNZIP and:
+docker load --input latestversion-1.0.0.tar
