ba4dabd9f77830b54fae836ed6e59b5894cbcbd8
Gorka Navarrete <gorkang@gmail.com>
2023-01-24 10:23:56 +0000

    update. Add win info

--- admin/create_docker.R
+++ admin/create_docker.R
@@ -2,29 +2,30 @@
 
 # DELETE ALL CACHE
 # docker builder prune
+# Clean renv cache: renv::clean()
 
 source("admin/helper-scripts-admin.R")
 PID = 999
 
-
-tictoc::tic()
 create_docker_container(PID = PID)
-tictoc::toc()
 #60 sec elapsed from CLEAN cache
 
-
 # Run project inside a docker container. Output in Downloads/jsPsychHelpeR/pid[PID]/
-tictoc::tic()
 file.remove(list.files(paste0("~/Downloads/jsPsychHelpeR", PID, "/outputs"), recursive = TRUE, full.names = TRUE))
-system(paste0("docker run --rm -d --name jspsychhelper -v ~/Downloads/jsPsychHelpeR", PID, "/outputs:/home/project/jsPsychHelpeR/outputs:rw pid", PID))
-tictoc::toc()
+system(paste0("docker run --rm -d --name pid", PID, " -v ~/Downloads/jsPsychHelpeR", PID, "/outputs:/home/project/jsPsychHelpeR/outputs:rw pid", PID))
+
+# Windows
+# In cmd:
+paste0("tar -xf pid", PID, ".tar.zip & ", 
+       "docker load input - & ",
+       r"(docker run --rm -d --name jspsychhelper -v %USERPROFILE%\Downloads\jsPsychHelpeR\outputs:/home/project/jsPsychHelpeR/outputs:rw pid)", PID
+       )
 
 
 # Creates a pid[PID].tar.zip file TO SHARE the container
-tictoc::tic()
 system(paste0("docker save pid", PID, " | zip > pid", PID, ".tar.zip"))
-tictoc::toc()
-# regular 1.89 GB #zip 699 MB # 83 sec elapsed
+# regular OLD 1.89 GB #zip 699 MB # 90 sec elapsed
+# regular 1.77 GB #zip 689 MB
 
 # xz, bz2, gz are slower and/or not significantly smaller
 
