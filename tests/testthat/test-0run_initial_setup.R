a55d7032e896a627b74844ba4c675b5e6a5b7268
G <gorkang@gmail.com>
2023-03-06 09:51:12 +0000

    Do not use snapshots, use trusted outputs

--- tests/testthat/test-0run_initial_setup.R
+++ tests/testthat/test-0run_initial_setup.R
@@ -33,11 +33,13 @@ testthat::test_that("Check that the project with 999.zip runs OK with run_initia
     # cat(TARGETS, file = targets_file, sep = "\n")
     # 
   
-  
+  # Run tar_make() in the TEMP_dir
   system(paste0("cd ", TEMP_dir, "; R ", TEMP_dir, " -e targets::tar_make\\(\\)"))
   
   FILES_root = list.files(TEMP_dir, recursive = FALSE)
   FILES_outputs = list.files(paste0(TEMP_dir, "/outputs/data/"), recursive = FALSE)
+  FILES_tasks = list.files(paste0(TEMP_dir, "/outputs/data/"), pattern = "df.*_sp", recursive = FALSE)
+  
 
   # Check that both _targets.R and output/ are created  
   testthat::expect_equal(sum(grepl("_targets.R|outputs", FILES_root)), 2)
@@ -48,14 +50,30 @@ testthat::test_that("Check that the project with 999.zip runs OK with run_initia
   testthat::expect_equal(sum(grepl("DF_joined.csv", FILES_outputs)), 1)
   
   
+  # prepare_TASKS
+  # IRI
+  # IRI_snap = readr::read_csv("inst/templates/tests/testthat/_snaps/snapshots/df_IRI.csv") #, col_types = readr::cols(id = readr::col_character())
+  # IRI = readr::read_csv(paste0(TEMP_dir, "/outputs/data/df_IRI.csv"))
+  # expect_equal(IRI_snap, IRI)
+  
+  seq_along(FILES_tasks) |> 
+    purrr::walk(~{
+      TASK = gsub("df_(.*)_sp\\.csv", "\\1", FILES_tasks[.x])
+      cli::cli_h1("{.x} - {TASK}")
+      SNAP = readr::read_csv(paste0(TEMP_dir, "/inst/templates/tests/testthat/_snaps/snapshots/df_", TASK,".csv")) #, col_types = readr::cols(id = readr::col_character())
+      OUT = readr::read_csv2(paste0(TEMP_dir, "/outputs/data/", FILES_tasks[.x]))
+      expect_equal(SNAP, OUT)
+    })
+  
+  
   # Check that the snapshots we have in /inst/templates/tests/_snaps/snapshots/ EQUAL to the ones created
-  SNAPS_new = list.files(paste0(TEMP_dir, "/tests/testthat/_snaps/snapshots/"), recursive = TRUE, full.names = TRUE)
-  SNAPS_stored = list.files(paste0(TEMP_dir, "/inst/templates/tests/testthat/_snaps/snapshots/"), recursive = TRUE, full.names = TRUE)
+  # SNAPS_new = list.files(paste0(TEMP_dir, "/tests/testthat/_snaps/snapshots/"), recursive = TRUE, full.names = TRUE)
+  # SNAPS_stored = list.files(paste0(TEMP_dir, "/inst/templates/tests/testthat/_snaps/snapshots/"), recursive = TRUE, full.names = TRUE)
   # SNAPS_new =  SNAPS_stored
   
-  testthat::expect_equal(length(SNAPS_stored), length(SNAPS_new))
+  # testthat::expect_equal(length(SNAPS_stored), length(SNAPS_new))
   
-  seq_len(length(SNAPS_stored)) |> 
-    purrr::walk(~ testthat::expect_equal(readLines(SNAPS_stored[.x]), readLines(SNAPS_new[.x])))
+  # seq_len(length(SNAPS_stored)) |> 
+  #   purrr::walk(~ testthat::expect_equal(readLines(SNAPS_stored[.x]), readLines(SNAPS_new[.x])))
   
 })
