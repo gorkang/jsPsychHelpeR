53dc72017d7ac5821caa08fdb634873cc018e7a7
G <gorkang@gmail.com>
2023-08-14 16:47:44 +0100

    sync_server_local should work now with a local tempdir where we unzipped the files we have, so only the new files will be downloaded

--- R/sync_server_local.R
+++ R/sync_server_local.R
@@ -24,7 +24,7 @@ sync_server_local <-
            all_messages = TRUE,
            list_credentials = NULL) {
     
-  # DEBUG
+    # DEBUG
     # pid = "999"
     # project_folder = getwd()
     # TEMP_DIR = tempdir(check = TRUE)
@@ -38,132 +38,153 @@ sync_server_local <-
     # all_messages = TRUE
     
     
-
-  # Parameters --------------------------------------------------------------
-
-  if (only_test == TRUE) {
-    extra_message = paste0(cli::col_red("THIS IS A dry-run"))
-    dry_run = " --dry-run "
-  } else {
-    extra_message = ""
-    dry_run = ""
-  }
-  
-  
-  if (exclude_csv) {
-    exclude_files = " --exclude='*.csv' "
-  } else {
-    exclude_files = ""
-  }
-  
-  if (delete_nonexistent) {
-    delete_nonexistent_files = " --delete "
-  } else {
-    delete_nonexistent_files = ""
-  }
-  
-  # Check and prepare local folder and path
-  if (!file.exists(local_folder)) dir.create(local_folder)
-  local_folder = normalizePath(here::here(local_folder))
-  local_folder_terminal = gsub(" ", "\\\\ ", local_folder)
-  
-  # CHECKS we have credentials and necessary software ------------------------
-  
-  # Get server credentials
-  if (is.null(list_credentials)) {
-    list_credentials = source(here::here(".vault/.credentials")) 
-    credentials_exist = file.exists(here::here(".vault/.credentials"))
-  } else {
-    credentials_exist = TRUE
-  }
-  
-  
-  # credentials_exist = file.exists(here::here(".vault/.credentials"))
-  SSHPASS = Sys.which("sshpass") # Check if sshpass is installed
-  RSYNC = Sys.which("rsync") # Check if rsync is installed
-  
-  if (credentials_exist) {
-    # sshpass and rsync installed (?)
-    if (SSHPASS != "" & RSYNC != "") { 
-      cli::cli_text(cli::col_green("{cli::symbol$tick} "), "rsync installed and credentials exist")
+    
+    # Parameters --------------------------------------------------------------
+    
+    if (only_test == TRUE) {
+      extra_message = paste0(cli::col_red("THIS IS A dry-run"))
+      dry_run = " --dry-run "
     } else {
-      cli::cli_abort("'sshpass' or 'rsync' not installed. Can't use `sync_server_local()`")
-    }
-  } else {
-    cli::cli_abort("Can't find server credentials in '.vault/.credentials'")
-  }
-  
-  
-  
-  # CHECK -------------------------------------------------------------------
-  
-  if (direction == "server_to_local") {
-    message_text = paste0(cli::col_yellow("Will sync: "), cli::col_silver("cscn.uai.cl/", server_folder, " -->> ", local_folder), "\n", extra_message)
-  } else if (direction == "local_to_server") {
-    message_text = paste0(cli::col_yellow("Will sync: "), cli::col_silver(local_folder, " -->> ", "cscn.uai.cl/", server_folder), "\n", extra_message)
-  } else {
-    cli::cli_abort("`direction` should be either 'server_to_local' or 'local_to_server'")
-  }
-  
-  if (all_messages == TRUE) cli::cli_h1("sync {direction} | delete_nonexistent {delete_nonexistent}")
-  
-  if (dont_ask == TRUE) {
-    out = 1
-    
-    if (all_messages == TRUE) {
-      cli::cli_par()
-      cli::cli_end()
-      # cli::cli_text(paste0(cli::col_yellow("BACKUP of full protocol WITHOUT data: "), cli::col_silver("cscn.uai.cl/", server_folder, " -->> data/protocol_", local_folder), "\n", extra_message))
-      cli::cli_text(paste0(cli::col_yellow("Will sync: "), cli::col_silver("cscn.uai.cl/", server_folder, " -->> ", local_folder), "\n", extra_message))
+      extra_message = ""
+      dry_run = ""
     }
     
-  } else {
-    out <- utils::menu(c("yes", "no"), title = cat(message_text))  
-  }
-  
-  
-  
-  # SYNC --------------------------------------------------------------------
-  
-  if (out == 1) {
+    
+    if (exclude_csv) {
+      exclude_files = " --exclude='*.csv' "
+    } else {
+      exclude_files = ""
+    }
+    
+    if (delete_nonexistent) {
+      delete_nonexistent_files = " --delete-during " # trying delete-during to check if works with ignore-existing #delete
+    } else {
+      delete_nonexistent_files = ""
+    }
+    
+    # Check and prepare local folder and path
+    if (!file.exists(local_folder)) dir.create(local_folder)
+    local_folder = normalizePath(here::here(local_folder))
+    local_folder_terminal = gsub(" ", "\\\\ ", local_folder)
+    FILES_in_local_folder = list.files(local_folder, recursive = TRUE)
+    
+    
+    # CHECKS we have credentials and necessary software ------------------------
+    
+    # Get server credentials
+    if (is.null(list_credentials)) {
+      list_credentials = source(here::here(".vault/.credentials")) 
+      credentials_exist = file.exists(here::here(".vault/.credentials"))
+    } else {
+      credentials_exist = TRUE
+    }
+    
+    
+    # credentials_exist = file.exists(here::here(".vault/.credentials"))
+    SSHPASS = Sys.which("sshpass") # Check if sshpass is installed
+    RSYNC = Sys.which("rsync") # Check if rsync is installed
+    
+    if (credentials_exist) {
+      # sshpass and rsync installed (?)
+      if (SSHPASS != "" & RSYNC != "") { 
+        cli::cli_text(cli::col_green("{cli::symbol$tick} "), "rsync installed and credentials exist")
+      } else {
+        cli::cli_abort("'sshpass' or 'rsync' not installed. Can't use `sync_server_local()`")
+      }
+    } else {
+      cli::cli_abort("Can't find server credentials in '.vault/.credentials'")
+    }
+    
+    
+    
+    # CHECK -------------------------------------------------------------------
     
     if (direction == "server_to_local") {
-      
-      # DOWNLOAD server to local
-      OUT = 
-        suppressWarnings(
-          system(
-            paste0('sshpass -p ', list_credentials$value$password, ' rsync -av ', dry_run, ' --rsh=ssh ', 
-                  delete_nonexistent_files, # Delete files from destination if they are NOT in origin anymore
-                  exclude_files, # exclude CSV files
-                  list_credentials$value$user, "@", list_credentials$value$IP, ":", list_credentials$value$main_FOLDER, server_folder, '/ ',
-                  here::here(local_folder_terminal), '/ '
-                  ), intern = TRUE
-            )
-        )
-      
-      
+      message_text = paste0(cli::col_yellow("Will sync: "), cli::col_silver("cscn.uai.cl/", server_folder, " -->> ", local_folder), "\n", extra_message)
     } else if (direction == "local_to_server") {
+      message_text = paste0(cli::col_yellow("Will sync: "), cli::col_silver(local_folder, " -->> ", "cscn.uai.cl/", server_folder), "\n", extra_message)
+    } else {
+      cli::cli_abort("`direction` should be either 'server_to_local' or 'local_to_server'")
+    }
+    
+    if (all_messages == TRUE) cli::cli_h1("sync {direction} | delete_nonexistent {delete_nonexistent}")
+    
+    if (dont_ask == TRUE) {
+      out = 1
       
-      # UPLOAD local to server
-      OUT = 
-        suppressWarnings(
-          system(
-            paste0('sshpass -p ', list_credentials$value$password, ' rsync -av ', dry_run, ' --rsh=ssh ', 
-                   here::here(local_folder_terminal), '/ ',
-                   list_credentials$value$user, "@", list_credentials$value$IP, ":", list_credentials$value$main_FOLDER, server_folder, '/ '
-          ), intern = TRUE)
-      )
+      if (all_messages == TRUE) {
+        cli::cli_par()
+        cli::cli_end()
+        # cli::cli_text(paste0(cli::col_yellow("BACKUP of full protocol WITHOUT data: "), cli::col_silver("cscn.uai.cl/", server_folder, " -->> data/protocol_", local_folder), "\n", extra_message))
+        cli::cli_text(paste0(cli::col_yellow("Will sync: "), cli::col_silver("cscn.uai.cl/", server_folder, " -->> ", local_folder), "\n", extra_message))
+      }
+      
+    } else {
+      out <- utils::menu(c("yes", "no"), title = cat(message_text))  
+    }
+    
+    
+    
+    # SYNC --------------------------------------------------------------------
+    
+    if (out == 1) {
+      
+      if (direction == "server_to_local") {
+        
+        # DOWNLOAD server to local
+        OUT = 
+          suppressWarnings(
+            system(
+              paste0('sshpass -p ', list_credentials$value$password, ' rsync -av ', dry_run, ' --rsh=ssh ', 
+                     delete_nonexistent_files, # Delete files from destination if they are NOT in origin anymore
+                     " --ignore-existing ", # Ignore existing files
+                     exclude_files, # exclude CSV files
+                     list_credentials$value$user, "@", list_credentials$value$IP, ":", list_credentials$value$main_FOLDER, server_folder, '/ ',
+                     here::here(local_folder_terminal), '/ '
+              ), intern = TRUE
+            )
+          )
+        
+        
+      } else if (direction == "local_to_server") {
+        
+        # UPLOAD local to server
+        OUT = 
+          suppressWarnings(
+            system(
+              paste0('sshpass -p ', list_credentials$value$password, ' rsync -av ', dry_run, ' --rsh=ssh ', 
+                     here::here(local_folder_terminal), '/ ',
+                     list_credentials$value$user, "@", list_credentials$value$IP, ":", list_credentials$value$main_FOLDER, server_folder, '/ '
+              ), intern = TRUE)
+          )
+        
+      }
+      
+    } else {
+      cli::cli_alert_info("Not doing anything...")
+    }
+    
+    if(is.null(OUT)) OUT = "Nothing done"
+    
+    
+    # Clean OUT so only filenames remain
+    OUT_clean = OUT[!grepl("^deleting|^receiving incremental file list|^sent|\\.\\/|^total", OUT)]
+    
+    # Show messages and create output
+    if (all_messages == TRUE) {
+
+      cli::cli_bullets_raw(OUT_clean)
+      cli::cli_alert_info("{length(FILES_in_local_folder)} files initially in {local_folder_terminal}")
+      cli::cli_alert_info("SYNCED {length(OUT_clean) - 1} NEW files to {local_folder_terminal}")
+      OUT_messages = ""
+      
+    } else {
+      
+      OUT_messages = paste0(length(FILES_in_local_folder), " files initially ",
+                            " | SYNCED ", length(OUT_clean) - 1, " NEW files to ", local_folder_terminal)
       
     }
     
-  } else {
-    cli::cli_alert_info("Not doing anything...")
+    return(OUT_messages)
+    
   }
- 
-  if(is.null(OUT)) OUT = "Nothing done"
-  
-  if (all_messages == TRUE) cli::cli_bullets_raw(OUT)
-  # return(OUT)
-
-}
