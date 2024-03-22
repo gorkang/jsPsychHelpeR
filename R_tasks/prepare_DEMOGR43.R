e5e759e42ec65a207f695f2672a401f736a98deb
G <gorkang@gmail.com>
2024-03-22 09:49:27 +0000

    add task

---
+++ R_tasks/prepare_DEMOGR43.R
@@ -0,0 +1,147 @@
+##' Prepare DEMOGR43
+##'
+##' Template for the functions to prepare specific tasks. Most of this file should not be changed
+##' Things to change: 
+##'   - Name of function: prepare_DEMOGR43 -> prepare_[value of short_name_scale_str] 
+##'   - dimensions parameter in standardized_names()
+##'   - 2 [ADAPT] chunks
+##'
+##' @title prepare_DEMOGR43
+##'
+##' @param short_name_scale_str 
+##' @param DF_clean
+##'
+##' @return
+##' @author gorkang
+##' @export
+prepare_DEMOGR43 <- function(DF_clean, short_name_scale_str) {
+
+  # DEBUG
+  # targets::tar_load_globals()
+  # jsPsychHelpeR::debug_function(prepare_DEMOGR43)
+  
+
+  
+  
+  # [ADAPT 1/3]: Items to ignore and reverse, dimensions -----------------------
+  # ****************************************************************************
+  
+  description_task = "" # Brief description here
+  
+  items_to_ignore = c("000") # Ignore these items: If nothing to ignore, keep as is
+  items_to_reverse = c("000") # Reverse these items: If nothing to reverse, keep as is
+  
+  ## NameDimension1, NameDimension2 should be the names of the dimensions
+  ## Inside each c() create a vector of the item numbers for the dimension
+  ## Add lines as needed. If there are no dimensions, keep as is
+  items_dimensions = list(
+    Edad = c("001"),
+    Sexo = c("002"),
+    Estudios = c("003")
+  )
+  
+  # [END ADAPT 1/3]: ***********************************************************
+  # ****************************************************************************
+  
+  
+  # Standardized names ------------------------------------------------------
+  names_list = standardized_names(short_name_scale = short_name_scale_str, 
+                                  dimensions = names(items_dimensions),
+                                  help_names = FALSE) # [KEEP as FALSE]
+  
+  # Create long -------------------------------------------------------------
+  DF_long_RAW = create_raw_long(DF_clean, 
+                                short_name_scale = short_name_scale_str, 
+                                numeric_responses = FALSE, # [TRUE or FALSE]
+                                is_experiment = FALSE, 
+                                keep_time = FALSE, # Keep time stamp for each response
+                                help_prepare = FALSE) # Show n of items, responses,... [CHANGE to TRUE to debug] 
+  
+  
+  # Create long DIR ------------------------------------------------------------
+  DF_long_DIR = 
+    DF_long_RAW %>% 
+    # If using keep_time = TRUE above, use this and add timestamp to the select() call
+    # dplyr::mutate(timestamp = as.POSIXlt(datetime, format = "%Y-%m-%dT%H%M%S")) |> 
+    dplyr::select(id, trialid, RAW) %>%
+    
+    
+    
+  # [ADAPT 2/3]: RAW to DIR for individual items -------------------------------
+  # ****************************************************************************
+  
+    # Transformations
+    dplyr::mutate(
+      DIR =
+       dplyr::case_when(
+         trialid == "DEMOGR43_001" ~ RAW,
+         
+         trialid == "DEMOGR43_002" & RAW == "Femenino" ~ "0",
+         trialid == "DEMOGR43_002" & RAW == "Masculino" ~ "1",
+         
+          trialid == "DEMOGR43_003" & RAW == "Bajo (Primaria o Secundaria)" ~ "1",
+          trialid == "DEMOGR43_003" & RAW == "Medio (Estudios formativas profesionales)" ~ "2",
+          trialid == "DEMOGR43_003" & RAW == "Alto (estudios superiores)" ~ "3",
+          
+          is.na(RAW) ~ NA_character_,
+          trialid %in% paste0(short_name_scale_str, "_", items_to_ignore) ~ NA_character_,
+          TRUE ~ "9999"
+        )
+    ) 
+    
+    
+  # [END ADAPT 2/3]: ***********************************************************
+  # ****************************************************************************
+    
+
+  # Create DF_wide_RAW_DIR -----------------------------------------------------
+  DF_wide_RAW =
+    DF_long_DIR %>% 
+    tidyr::pivot_wider(
+      names_from = trialid, 
+      values_from = c(RAW, DIR),
+      names_glue = "{trialid}_{.value}") %>% 
+    
+    # NAs for RAW and DIR items
+    dplyr::mutate(!!names_list$name_RAW_NA := rowSums(is.na(select(., -matches(paste0(short_name_scale_str, "_", items_to_ignore, "_RAW")) & matches("_RAW$")))),
+           !!names_list$name_DIR_NA := rowSums(is.na(select(., -matches(paste0(short_name_scale_str, "_", items_to_ignore, "_DIR")) & matches("_DIR$")))))
+
+
+  
+  # [ADAPT 3/3]: Scales and dimensions calculations ----------------------------
+  # ****************************************************************************
+  
+  # Reliability -------------------------------------------------------------
+  # REL1 = auto_reliability(DF_wide_RAW, short_name_scale = short_name_scale_str, items = items_dimensions[[1]])
+  # items_RELd1 = REL1$item_selection_string
+    
+  
+  # [USE STANDARD NAMES FOR Scales and dimensions: names_list$name_DIRd[1], names_list$name_DIRt,...] 
+  # CHECK with: create_formulas(type = "dimensions_DIR", functions = "sum", names(items_dimensions))
+  DF_wide_RAW_DIR =
+    DF_wide_RAW %>% 
+    dplyr::mutate(
+
+      # [CHECK] Using correct formula? rowMeans() / rowSums()
+      
+      # Score Dimensions (see standardized_names(help_names = TRUE) for instructions)
+      !!names_list$name_DIRd[1] := get(paste0(short_name_scale_str, "_", items_dimensions[[1]], "_DIR")),
+      !!names_list$name_DIRd[2] := get(paste0(short_name_scale_str, "_", items_dimensions[[2]], "_DIR")),
+      !!names_list$name_DIRd[3] := get(paste0(short_name_scale_str, "_", items_dimensions[[3]], "_DIR"))
+      
+    )
+    
+  # [END ADAPT 3/3]: ***********************************************************
+  # ****************************************************************************
+
+
+  # CHECK NAs -------------------------------------------------------------------
+  check_NAs(DF_wide_RAW_DIR)
+  
+  # Save files --------------------------------------------------------------
+  save_files(DF_wide_RAW_DIR, short_name_scale = short_name_scale_str, is_scale = TRUE)
+  
+  # Output of function ---------------------------------------------------------
+  return(DF_wide_RAW_DIR) 
+ 
+}
